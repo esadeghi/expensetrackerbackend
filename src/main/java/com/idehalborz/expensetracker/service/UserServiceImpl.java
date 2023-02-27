0c7eea485af75fe472678d0eac7e79e1d0a97f56
Ehsan Sadeghi <esadeghi@gmail.com>
2023-02-27 13:25:09 -0500

    Add User Service implementation

---
+++ src/main/java/com/idehalborz/expensetracker/service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.idehalborz.expensetracker.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
