021ef4fbfa307ab11866df81f6ea86f7faaad4f0
Ehsan Sadeghi <esadeghi@gmail.com>
2023-02-27 13:49:45 -0500

    Refactor User entity to AppUser

--- src/main/java/com/idehalborz/expensetracker/service/UserService.java
+++ src/main/java/com/idehalborz/expensetracker/service/UserService.java
@@ -1,6 +1,8 @@
 package com.idehalborz.expensetracker.service;
 
+import com.idehalborz.expensetracker.dto.UserDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
+    UserDto createUser(UserDto userDto);
 }
